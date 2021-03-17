8d0e102e6b23b2ce182899244317a850c4b7a0ef
shaswata965 <shaswata965@gmail.com>
2021-03-17 16:53:03 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,68 @@
 const express = require("express");
-const bodyParser = require ("body-parser");
-const date = require(__dirname +"/date.js");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
-var newTasks = [];
-var workTasks = [];
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
-app.set("view engine","ejs");
-
-app.get("/",function(req, res){
-    let  day = date.getDate();
-  res.render("list",{listTitle:day, Task:newTasks});
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/sign-up.html");
 });
 
-app.post("/",function(req,res){
-   var newTask = req.body.addItem;
-    // newTask = req.body.addItem;
-  if(req.body.switch === "Work" ){
-      workTasks.push(newTask);
-      res.redirect("/work");
-  }else{
-       newTasks.push(newTask);
-         res.redirect("/");
+app.post("/", function(req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailAddress = req.body.emailAddress;
+
+  const data = {
+    members: [{
+      email_address: emailAddress,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
   };
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us1.api.mailchimp.com/3.0/lists/b5f3f0b37f";
+  const options = {
+    method: "POST",
+    auth: "shaswata1:a49a4e9755c4b691533ef2138057e201-us1"
+  };
+  const request = https.request(url, options, function(response) {
+    const code =  response.statusCode;
+    if(code===200){
+        res.sendFile(__dirname + "/success.html");
+    }
+    else{
+          res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+
+  });
+  app.post("/failure", function(req,res){
+    res.sendFile(__dirname + "/sign-up.html");
+  })
+
+  request.write(jsonData);
+  request.end();
+
+
 
 });
 
-app.get("/work", function(req,res){
-
-  res.render("list",{listTitle:"Work List", Task:workTasks});
+app.listen(process.env.PORT, function() {
+  console.log("Server is running on port 3000");
 });
 
-app.listen(3000, function(){
-  console.log("The server is listening on port 3000");
-})
+// list id :b5f3f0b37f
+// apikey :a49a4e9755c4b691533ef2138057e201-us1
